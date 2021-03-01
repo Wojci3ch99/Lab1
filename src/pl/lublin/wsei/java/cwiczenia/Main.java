fdb9ca73e3628f9a6398b98b01717e9d25458e0a
Wojci3ch99 <Wojciech9905@gmail.com>
2021-03-01 22:23:53 +0100

    Drugi commit

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -18,5 +18,24 @@ public class Main {
 
         System.out.println("Hello Java world");
 
+        System.out.print("Ala\n");
+        System.out.print("ma\n");
+        System.out.print("kota\n");
+
+        int a = 3;
+        double b = 4.21;
+        String s = "jakiś tekst";
+
+        System.out.printf("a = %d, b = %.2f, s = %20s %n",a,b,s);
+
+        System.out.printf("Nazwaliśmy to \"witaminą B3\" \n");
+
+        System.out.printf("alfa\tsin(alfa)\n");
+        for (int i = 0; i < 370; i+=10)
+            System.out.printf("%d\t%f\t\n", i, Math.sin(i/360.0*2*Math.PI));
+
+
+
+
     }
 }
