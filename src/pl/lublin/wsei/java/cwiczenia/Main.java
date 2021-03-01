86553c52c13943ba9d068e59240f795e0d558329
Wojci3ch99 <Wojciech9905@gmail.com>
2021-03-01 21:47:30 +0100

    Initial commit

---
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -0,0 +1,22 @@
+package pl.lublin.wsei.java.cwiczenia;
+
+import java.sql.SQLOutput;
+
+public class Main {
+
+    /**
+     @param
+     @return zwraca wynik programu
+     */
+
+    public static void main(String[] args) {
+        // to jest komentarz jednolinikjowy
+        /* to jest
+            komentarz
+            wielolinikjowy
+        */
+
+        System.out.println("Hello Java world");
+
+    }
+}
